92404d2b020670f960a16f8c054fa31aff0963c8
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-06-01 21:42:29 -0400

    api slice

--- example-app/src/features/api/apiSlice.js
+++ example-app/src/features/api/apiSlice.js
@@ -1,1 +1,38 @@
-// where we are going to create methods 
+// where we are going to create our methods to interact with the api it essentially replaces axios 
+// and pulls that code out of our component logicand over here in a seperate api slice 
+// how we name things in redux 
+
+
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+// createApi 
+// fetchBaseQuery 
+// imports tools for react 
+
+
+
+// object 
+export const apiSlice = createApi({
+    
+    // first thing we do is define the reducer path 
+    // defaults to this (good to include)
+    reducerPath: 'api',
+    // baseQuery uses fetchBaseQuery 
+    // baseURL: looks very familiar with axios (using json server)
+    baseQuery: fetchBaseQuery({baseUrl:  'http://localhost:3500' }),
+
+    //we're defining endpoints for the api to interact with (builder) cases 
+    endpoints : (builder) => ({
+        // methods 
+        
+
+        // get all of the todos
+        getTodos: builder.query({
+            query : () => '/todos', // gets it using an http get method 
+        })
+    })
+})
+
+export const {
+    useGetTodosQuery
+} = apiSlice
--- example-app/src/index.js
+++ example-app/src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
 
+
+// context provider 
+// we need to provide our slice to this app 
+
+import { ApiProvider } from "@reduxjs/toolkit/query/react";
+import { apiSlice } from "./features/api/apiSlice";
+
+// wrapping App in the ApiProvider
 ReactDOM.createRoot(document.getElementById('root'))
     .render(
         <React.StrictMode>
-            <App />
+            <ApiProvider api = {apiSlice}>
+                <App />
+            </ApiProvider>
         </React.StrictMode>
     );
